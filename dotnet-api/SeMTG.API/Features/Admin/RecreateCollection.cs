d14df02000c078e0d03a9934bdf8f1619b2ae296
Robin Landerloos <rlanderloos@gmail.be>
2025-04-26 11:42:37 +0200

    Added a route for recreating a collection

---
+++ dotnet-api/SeMTG.API/Features/Admin/RecreateCollection.cs
@@ -0,0 +1,17 @@
+﻿using SeMTG.API.Embedding;
+using SeMTG.API.Qdrant;
+
+namespace SeMTG.API.Features.Admin;
+
+public static class RecreateCollection
+{
+	public static void MapRecreateCollection(this IEndpointRouteBuilder builder)
+	{
+		builder.MapPost("/admin/recreate-collection", RecreateCollectionAsync);
+	}
+
+	public static async Task RecreateCollectionAsync(IEmbeddingService embeddingService, QdrantService qdrantService)
+	{
+		await qdrantService.RecreateCollectionAsync();
+	}
+}
